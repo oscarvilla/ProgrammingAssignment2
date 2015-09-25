f2a478e9a3c97a79505c29f545e2cc51c4ae2a64
oscarvilla <villaoscarad@gmail.com>
2015-09-24 22:06:36 -0500

    Function with comments, Trying to make it readable and understablle to my self

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,48 @@
 ## This pair of functions cache the inverse of a matrix
+##If matrix inverse has been calculated, cached it. If not, calculate it
 
 ## This function creates a special "matrix" object that can cache its
-## inverse
+## inverse if these has been calculated
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
+        ##'m' is the inverse of the matrix
+        m <- NULL ##'m' is stated as NULL
         set <- function(y) {
-                x <<- y
-                m <<- NULL
+                ##the value of 'y' is passed to 'x' on the external
+                ##environment to be calculate by the another function
+                ##wich call to m$set
+                x <<- y 
+                m <<- NULL ##then 'm' is set up NULL
         }
+        ##'y' has been received as 'x' from the previous function
         get <- function() x
-        setinv <- function(t) m <<- t
+        ##the inverse of matrix x is calculate through an anonymous fun
+        setinv <- function(inv) m <<- inv
         getinv <- function() m
         list(set = set, get = get,
              setinv = setinv,
              getinv = getinv)
+        ##arguments calculations are passed as a list 'm$set', 'm$get'
+        ##'m$getinv' and 'm$setinv'
 }
 
 
-## Write a short comment describing this function
+##This function cache the inverse of the matrix 'x' is these has been
+##calculated. If not, calculate it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ##the value of the inverse of the matrix 'x' is cached as 
+        ##argument
         m <- x$getinv()
+        ##if the inverse is no NULL, that means already was calculated
+        ##and is cached and returned with a statement message
         if(!is.null(m)) {
                 message("getting cached matrix data")
                 return(m)
         }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setinv(m)
+        ##if has not been calculated, it is done
+        data <- x$get() ##captures 'x' calling to another function
+        m <- solve(data, ...) ##calculates the inverse of 'x'
+        x$setinv(m) ##pass the inverse to the function wich states it
         m
 }
